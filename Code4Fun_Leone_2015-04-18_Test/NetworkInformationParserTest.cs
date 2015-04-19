4c317ad49ac065adb05d306704877480c63bafe3
giuseppleon <giuseppleon@gmail.com>
2015-04-19 16:55:41 +0200

    Implement unit test for parser

--- Code4Fun_Leone_2015-04-18_Test/NetworkInformationParserTest.cs
+++ Code4Fun_Leone_2015-04-18_Test/NetworkInformationParserTest.cs
@@ -2,15 +2,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Code4Fun_Leone_2015_04_18_Test
 {
-    
-    
-    /// <summary>
-    ///Classe di test per NetworkInformationParserTest.
-    ///Creata per contenere tutti gli unit test NetworkInformationParserTest
-    ///</summary>
     [TestClass()]
     public class NetworkInformationParserTest
     {
@@ -18,10 +13,6 @@ namespace Code4Fun_Leone_2015_04_18_Test
 
         private TestContext testContextInstance;
 
-        /// <summary>
-        ///Ottiene o imposta il contesto dei test, che fornisce
-        ///funzionalità e informazioni sull'esecuzione dei test corrente.
-        ///</summary>
         public TestContext TestContext
         {
             get
@@ -34,68 +25,144 @@ namespace Code4Fun_Leone_2015_04_18_Test
             }
         }
 
-        #region Attributi di test aggiuntivi
-        // 
-        //Durante la scrittura dei test è possibile utilizzare i seguenti attributi aggiuntivi:
-        //
-        //Utilizzare ClassInitialize per eseguire il codice prima di eseguire il primo test della classe
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Utilizzare ClassCleanup per eseguire il codice dopo l'esecuzione di tutti i test di una classe
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Utilizzare TestInitialize per eseguire il codice prima di eseguire ciascun test
-        //
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //Utilizzare TestCleanup per eseguire il codice dopo l'esecuzione di ciascun test
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
-
         /// <summary>
-        ///Test per ParseFlatFile
-        ///</summary>
+        /// Test if the NetworkInformation is properly parsed (normal use)
+        /// </summary>
         [TestMethod()]
         [DeploymentItem("Code4Fun_Leone_2015-04-18.dll")]
-        public void ParseFlatFileTest()
+        public void ParseFlatFile_ParseFlatFile()
         {
-            string filename = string.Empty; // TODO: Eseguire l'inizializzazione a un valore appropriato
-            char delimiter = '\0'; // TODO: Eseguire l'inizializzazione a un valore appropriato
-            NetworkInformation expected = null; // TODO: Eseguire l'inizializzazione a un valore appropriato
+            string startupPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(startupPath, @"..\..\Test_Data\1.tsv");
+            Console.WriteLine(filename);
+            char delimiter = '\t';
+            NetworkInformation expected = new NetworkInformation(1, 3, 50);
             NetworkInformation actual;
             actual = NetworkInformationParser_Accessor.ParseFlatFile(filename, delimiter);
-            Assert.AreEqual(expected, actual);
-            Assert.Inconclusive("Verificare la correttezza del metodo di test.");
+            Assert.IsTrue(expected.Equals(actual));
+
         }
 
         /// <summary>
-        ///Test per ParseFlatFiles
-        ///</summary>
+        /// Test if the method throws an exception when the file is incomplete
+        /// </summary>
         [TestMethod()]
         [DeploymentItem("Code4Fun_Leone_2015-04-18.dll")]
-        public void ParseFlatFilesTest()
+        [ExpectedException(typeof(System.Exception))]
+        public void ParseFlatFile_IncompleteFile()
         {
-            string path = string.Empty; // TODO: Eseguire l'inizializzazione a un valore appropriato
-            string searchPattern = string.Empty; // TODO: Eseguire l'inizializzazione a un valore appropriato
-            char delimiter = '\0'; // TODO: Eseguire l'inizializzazione a un valore appropriato
-            List<NetworkInformation> expected = null; // TODO: Eseguire l'inizializzazione a un valore appropriato
+            string startupPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(startupPath, @"..\..\Test_Data2\3.tsv");
+            Console.WriteLine(filename);
+            char delimiter = '\t';
+            NetworkInformationParser_Accessor.ParseFlatFile(filename, delimiter);
+
+        }
+
+        /// <summary>
+        /// Test if the method throws an exception when the file does not exists
+        /// </summary>
+        [TestMethod()]
+        [DeploymentItem("Code4Fun_Leone_2015-04-18.dll")]
+        [ExpectedException(typeof(System.IO.FileNotFoundException))]
+        public void ParseFlatFile_FileNotFound()
+        {
+            string startupPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(startupPath, @"..\..\Test_Data2\filenotfound.tsv");
+            Console.WriteLine(filename);
+            char delimiter = '\t';
+            NetworkInformationParser_Accessor.ParseFlatFile(filename, delimiter);
+
+        }
+
+        /// <summary>
+        /// Test if the method throws an exception when the file contains a duplicate key
+        /// </summary>
+        [TestMethod()]
+        [DeploymentItem("Code4Fun_Leone_2015-04-18.dll")]
+        [ExpectedException(typeof(System.Exception))]
+        public void ParseFlatFile_DuplicateKey()
+        {
+            string startupPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filename = Path.Combine(startupPath, @"..\..\Test_Data3\4.tsv");
+            Console.WriteLine(filename);
+            char delimiter = '\t';
+            NetworkInformationParser_Accessor.ParseFlatFile(filename, delimiter);
+
+        }
+
+        /// <summary>
+        /// Test if all the NetworkInformations are properly parsed (normal use)
+        /// </summary>
+        [TestMethod()]
+        [DeploymentItem("Code4Fun_Leone_2015-04-18.dll")]
+        public void ParseFlatFiles_ParseFlatFiles()
+        {
+            string startupPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string path = Path.Combine(startupPath, @"..\..\Test_Data");
+            Console.WriteLine(path);
+            string searchPattern = "*.tsv";
+            char delimiter = '\t';
+            List<NetworkInformation> expected = new List<NetworkInformation>();
+            expected.Add(new NetworkInformation(1, 3, 50));
+            expected.Add(new NetworkInformation(5, 7, 100));
             List<NetworkInformation> actual;
             actual = NetworkInformationParser_Accessor.ParseFlatFiles(path, searchPattern, delimiter);
-            Assert.AreEqual(expected, actual);
-            Assert.Inconclusive("Verificare la correttezza del metodo di test.");
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.IsTrue(expected.Exists(x => x.Equals(actual[0])));
+            Assert.IsTrue(expected.Exists(x => x.Equals(actual[1])));
+        }
+
+        /// <summary>
+        /// Test if the method throws an exception when the directory does not exists
+        /// </summary>
+        [TestMethod()]
+        [DeploymentItem("Code4Fun_Leone_2015-04-18.dll")]
+        [ExpectedException(typeof(System.IO.DirectoryNotFoundException))]
+        public void ParseFlatFiles_DirectoryNotFound()
+        {
+            string startupPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string path = Path.Combine(startupPath, @"..\..\directorynotfound");
+            Console.WriteLine(path);
+            string searchPattern = "*.tsv";
+            char delimiter = '\t';
+            NetworkInformationParser_Accessor.ParseFlatFiles(path, searchPattern, delimiter);
+
+        }
+
+
+        /// <summary>
+        /// Test if the method throws an exception when a file is incomplete
+        /// </summary>
+        [TestMethod()]
+        [DeploymentItem("Code4Fun_Leone_2015-04-18.dll")]
+        [ExpectedException(typeof(System.Exception))]
+        public void ParseFlatFiles_IncompleteFile()
+        {
+            string startupPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string path = Path.Combine(startupPath, @"..\..\Test_Data2");
+            Console.WriteLine(path);
+            string searchPattern = "*.tsv";
+            char delimiter = '\t';
+            NetworkInformationParser_Accessor.ParseFlatFiles(path, searchPattern, delimiter);
+
+        }
+
+        /// <summary>
+        /// Test if the method throws an exception when a file contains a duplicate key
+        /// </summary>
+        [TestMethod()]
+        [DeploymentItem("Code4Fun_Leone_2015-04-18.dll")]
+        [ExpectedException(typeof(System.Exception))]
+        public void ParseFlatFiles_DuplicateKey()
+        {
+            string startupPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string path = Path.Combine(startupPath, @"..\..\Test_Data3");
+            Console.WriteLine(path);
+            string searchPattern = "*.tsv";
+            char delimiter = '\t';
+            NetworkInformationParser_Accessor.ParseFlatFiles(path, searchPattern, delimiter);
+
         }
     }
 }
